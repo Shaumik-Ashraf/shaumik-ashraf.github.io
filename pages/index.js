5bb22dfe375173c5138986ed218e5e8695e9a97f
Shaumik-Ashraf <a.shaumik@gmail.com>
2026-03-22 19:06:38 -0700

    wip: matter-js engine

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-export default function Page() {
+export default function Page(props) {
   return <div className="px-2 my-5 text-center">
 				   <h1 className="display-5 fw-bold text-primary">Development in Progress</h1>
-				   <p className="text-lead text-secondary">Please come back later.</p>
+             <div ref={props.canvas}
+                  onMouseDown={props.handleMouseDown}
+                  onMouseUp={props.handleMouseUp}
+                  className="w-100"
+                  height="1000px" >
+		         </div>
          </div>
 }
