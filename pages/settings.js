9d3092319312fc04afd7f97339bba2bb43b05aea
Shaumik-Ashraf <a.shaumik@gmail.com>
2026-03-22 22:50:50 -0700

    wip: fix settings modal

--- pages/settings.js
+++ pages/settings.js
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 export default function SettingsPage() {
   const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => { setMounted(true); }, []);
+
   const handleClose = () => router.push("/");
+
+  if (!mounted) return null;
 
   return (
     <Modal show onHide={handleClose} centered>
